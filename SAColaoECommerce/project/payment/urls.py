f5880622beff1e2873965e1f905c447fbb81d4dd
Renato Soma <renato-ys@hotmail.com>
2018-06-10 16:41:17 -0300

    Criar codigo de rastreio

--- SAColaoECommerce/project/payment/urls.py
+++ SAColaoECommerce/project/payment/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
     path('pay_by_slip/', views.pay_by_slip, name='slip'),
-    path('all_cards/', views.all_cards, name='all_cards'),
-    path('card_by_number/', views.card_by_number, name='card_by_number'),
+    # path('all_cards/', views.all_cards, name='all_cards'),
+    # path('card_by_number/', views.card_by_number, name='card_by_number'),
     path('pay_by_credit_card/', views.pay_by_credit_card, name='credit'),
     path('slip_status/', views.pay_by_credit_card, name='slip_status'),
-    path('invoice/', views.pay_by_credit_card, name='invoice'),
-    path('installments/', views.installments, name='installments')
-
+    path('get_track_id/', views.get_track_id, name='get_track_id')
+    # path('invoice/', views.pay_by_credit_card, name='invoice'),
+    # path('installments/', views.installments, name='installments')
 ]
--- SAColaoECommerce/project/payment/views.py
+++ SAColaoECommerce/project/payment/views.py
@@ -2,9 +2,43 @@ import requests
 import json
 from django.http import HttpResponse, HttpRequest, JsonResponse
 
+# Permitir requisoces do Postman
+from django.views.decorators.csrf import csrf_exempt
+
 base_url = 'https://payment-server-mc851.herokuapp.com'
+logistica_url = 'https://hidden-basin-50728.herokuapp.com/'
+logistica_api_key = '3b8e8f59-6425-5b97-acc5-6fda92b7ada0'
+cepOrigem = "13083-852"
 
+def format_json(requisition):
+    body_unicode = requisition.body.decode('utf-8')
+    return json.loads(body_unicode)
 
+# Retorno do codigo de rastreio para o produto
+@csrf_exempt
+def get_track_id(requisition):
+    body = format_json(requisition)
+    payload = {
+        "idProduto": body['idProduto'],
+        "tipoEntrega": body['tipoEntrega'],
+        "cepOrigem": cepOrigem,
+        "cepDestino": body['cepDestino'],
+        "peso": body['peso'],
+        "tipoPacote": "Caixa",
+        "altura": body['altura'],
+        "largura": body['largura'],
+        "comprimento": body['comprimento'],
+        "apiKey": logistica_api_key
+    }
+    response = requests.post('https://hidden-basin-50728.herokuapp.com/cadastrarentrega', json=payload).json()
+    return JsonResponse({
+        'message'        : "Codigo de rastreio",
+        'status'         : response['status'],
+        'content'        : response['codigoRastreio']
+        }
+    )
+
+@csrf_exempt
 def pay_by_credit_card(request):
     url = '/payments/creditCard'
     payload = json.loads(request.body)
@@ -21,15 +55,15 @@ def pay_by_credit_card(request):
 
     return JsonResponse(response)
 
-def installments(request):
-    url = '/payments/creditCard/installments'
-    payload = json.loads(request.body)
+# def installments(request):
+#     url = '/payments/creditCard/installments'
+#     payload = json.loads(request.body)
 
-    module_request = requests.post(base_url + url, json=payload)
+#     module_request = requests.post(base_url + url, json=payload)
 
-    response = module_request.json()
+#     response = module_request.json()
 
-    return JsonResponse(response)
+#     return JsonResponse(response)
 
 def pay_by_slip(request):
     url = '/payments/bankTicket'
@@ -51,31 +85,31 @@ def slip_status(request):
 
     return JsonResponse(response)
 
-def invoice(request):
-    url = '/invoice'
-    payload = json.loads(request.body)
+# def invoice(request):
+#     url = '/invoice'
+#     payload = json.loads(request.body)
 
-    module_request = requests.post(base_url + url, json=payload)
+#     module_request = requests.post(base_url + url, json=payload)
 
-    response = module_request.json()
+#     response = module_request.json()
 
-    return JsonResponse(response)
+#     return JsonResponse(response)
 
-def all_cards(request):
-    url = '/creditCard'
+# def all_cards(request):
+#     url = '/creditCard'
 
-    module_request = requests.get(base_url + url)
+#     module_request = requests.get(base_url + url)
 
-    response = module_request.json()
+#     response = module_request.json()
 
-    return JsonResponse(response)
+#     return JsonResponse(response)
 
-def card_by_number(request):
-    url = '/creditCard/'
-    payload = json.loads(request.body)
+# def card_by_number(request):
+#     url = '/creditCard/'
+#     payload = json.loads(request.body)
 
-    module_request = requests.get(base_url + url + str(payload['number']), json=payload)
+#     module_request = requests.get(base_url + url + str(payload['number']), json=payload)
 
-    response = module_request.json()
+#     response = module_request.json()
 
-    return JsonResponse(response)
+#     return JsonResponse(response)
