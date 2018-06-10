f4dfbd08bf2ab95303ea4a88dec33d3efd243e2e
Renato Soma <renato-ys@hotmail.com>
2018-06-10 18:43:11 -0300

    Integ cart and Payment

--- SAColaoECommerce/project/payment/urls.py
+++ SAColaoECommerce/project/payment/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     # path('all_cards/', views.all_cards, name='all_cards'),
     # path('card_by_number/', views.card_by_number, name='card_by_number'),
     path('pay_by_credit_card/', views.pay_by_credit_card, name='credit'),
-    path('slip_status/', views.pay_by_credit_card, name='slip_status'),
-    path('get_track_id/', views.get_track_id, name='get_track_id')
+    # path('slip_status/', views.slip_status, name='slip_status'),
+    path('get_track_id/', views.get_track_id, name='get_track_id'),
+	path('get_total_value/', views.get_total_value, name='get_total_value')    
     # path('invoice/', views.pay_by_credit_card, name='invoice'),
     # path('installments/', views.installments, name='installments')
 ]
