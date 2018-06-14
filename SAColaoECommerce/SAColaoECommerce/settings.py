3c3bcd8744688e6249b1d40a1d8d2f2c4cb189cd
Renato Soma <renato-ys@hotmail.com>
2018-06-13 21:26:35 -0300

    Update base.py

--- SAColaoECommerce/SAColaoECommerce/settings.py
+++ SAColaoECommerce/SAColaoECommerce/settings.py
@@ -22,11 +22,11 @@ SECRET_KEY = '4#u9-nwtlgcgntuvx1&&yr9nz(d*u6$thqzjm!wt$3h-kw#yed'
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
 #ALLOWED_HOSTS = ['*']
-#ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -87,8 +87,8 @@ WSGI_APPLICATION = 'SAColaoECommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'USER': 'name',
         'PASSWORD': '',
