ae800434a14150215a942a97d98d182d69aee2d3
Renato Soma <renato-ys@hotmail.com>
2018-06-13 21:43:20 -0300

    Update base.py

--- SAColaoECommerce/SAColaoECommerce/settings.py
+++ SAColaoECommerce/SAColaoECommerce/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 CART_SESSION_ID = 'cart_session_id'
 
